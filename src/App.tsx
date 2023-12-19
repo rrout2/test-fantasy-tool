97be4e969e7343439e2497f86aa115e6750addd0
Rishav Rout <rout.rishav@gmail.com>
2023-12-19 15:22:41 -0800

    tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import React, {useEffect} from 'react';
 // import logo from './logo.svg';
 import './App.css';
-
+import {readFileSync} from 'fs';
 function App() {
-  // useEffect(() => {
-  //   fs.readFileSync('players/all_players.json', 'utf-8');
-  // });
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        readFileSync('players/all_players.json', 'utf-8');
+    });
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
