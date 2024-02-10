2701edd5d24799ecc42327672a265448026a300a
Rishav Rout <rout.rishav@gmail.com>
2024-02-09 20:09:27 -0800

    routes and ranker

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import FootballRanker from './football-ranker/FootballRanker';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/test-fantasy-tool" element={<App />}></Route>
+                <Route path="/" element={<App />}></Route>
+                <Route
+                    path="/football-ranker"
+                    element={<FootballRanker />}
+                ></Route>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
