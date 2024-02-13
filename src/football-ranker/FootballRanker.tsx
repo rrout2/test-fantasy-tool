5730a4d5d9c0282141592edd20e6fbd233c7207e
Rishav Rout <rout.rishav@gmail.com>
2024-02-13 14:18:32 -0800

    decent working, pre-toggle

--- src/football-ranker/FootballRanker.tsx
+++ src/football-ranker/FootballRanker.tsx
@@ -1,7 +1,13 @@
 import React, {ChangeEvent, useEffect, useMemo, useState} from 'react';
 import nflPlayersJson from './nfl_players.json';
 import './FootballRanker.css';
-import {Button} from '@mui/material';
+import {
+    Button,
+    Typography,
+    Switch,
+    FormGroup,
+    FormControlLabel,
+} from '@mui/material';
 import {
     MRT_ColumnDef,
     MRT_Row,
@@ -20,10 +26,25 @@ type PlayerJson = {
 
 const FANTASY_POSITIONS = new Set(['QB', 'RB', 'WR', 'TE']);
 
+enum Table {
+    None = 'no table',
+    Rankings = 'rankings table',
+    RemainingPlayers = 'remaining players table',
+}
+
 export default function FootballRanker() {
     const [rankings, setRankings] = useState<PlayerJson[]>([]);
-    const [players, setPlayers] = useState<Map<number, PlayerJson>>(new Map());
-
+    const [allPlayers, setAllPlayers] = useState<Map<number, PlayerJson>>(
+        new Map()
+    );
+    const [remainingPlayers, setRemainingPlayers] = useState<PlayerJson[]>([]);
+    const [draggingRow, setDraggingRow] = useState<MRT_Row<PlayerJson> | null>(
+        null
+    );
+    const [hoveredRow, setHoveredRow] = useState<MRT_Row<PlayerJson> | null>(
+        null
+    );
+    const [hoveredTable, setHoveredTable] = useState<Table>(Table.None);
     const columns = useMemo<MRT_ColumnDef<PlayerJson>[]>(
         () => [
             {
@@ -45,6 +66,9 @@ export default function FootballRanker() {
         []
     );
 
+    const [displayRemainingPlayersTable, setDisplayRemainingPlayersTable] =
+        useState(true);
+
     useEffect(() => {
         loadPlayers();
     }, []);
@@ -58,7 +82,7 @@ export default function FootballRanker() {
         players.forEach(player => {
             mappedPlayers.set(player.player_id, player);
         });
-        setPlayers(mappedPlayers);
+        setAllPlayers(mappedPlayers);
         setRankings(players);
     }
 
@@ -90,12 +114,18 @@ export default function FootballRanker() {
         fileReader.readAsText(e.target.files[0], 'UTF-8');
         fileReader.onload = e => {
             if (!e.target) return;
-            const raw = JSON.parse(e.target.result as string) as number[];
+            const rawPlayerIds = JSON.parse(
+                e.target.result as string
+            ) as number[];
+            const remaining = new Set(allPlayers.values());
             setRankings(
-                raw.map(playerId => {
-                    return players.get(playerId)!;
+                rawPlayerIds.map(playerId => {
+                    const player = allPlayers.get(playerId)!;
+                    remaining.delete(player);
+                    return player;
                 })
             );
+            setRemainingPlayers(Array.from(remaining));
         };
     }
 
@@ -113,6 +143,33 @@ export default function FootballRanker() {
         );
     }
 
+    function startNewRankings() {
+        setRankings([]);
+        setRemainingPlayers(Array.from(allPlayers.values()));
+    }
+
+    function startNewButton() {
+        return <Button onClick={startNewRankings}>Start New</Button>;
+    }
+
+    function remainingPlayersToggle() {
+        return (
+            <FormGroup>
+                <FormControlLabel
+                    control={
+                        <Switch
+                            checked={displayRemainingPlayersTable}
+                            onChange={(e, checked) =>
+                                setDisplayRemainingPlayersTable(checked)
+                            }
+                        />
+                    }
+                    label="Remaining Players Table"
+                />
+            </FormGroup>
+        );
+    }
+
     function rankingComponent() {
         return (
             <MaterialReactTable
@@ -122,17 +179,158 @@ export default function FootballRanker() {
                     data: rankings,
                     enableRowOrdering: true,
                     enableSorting: false,
+                    renderTopToolbarCustomActions: () => (
+                        <Typography variant="h5">Rankings</Typography>
+                    ),
+                    onDraggingRowChange: setDraggingRow,
+                    state: {draggingRow},
                     muiRowDragHandleProps: ({table}) => ({
+                        onDragStart: () => {
+                            setHoveredTable(Table.Rankings);
+                        },
                         onDragEnd: () => {
-                            const {draggingRow, hoveredRow} = table.getState();
-                            if (hoveredRow && draggingRow) {
-                                rankings.splice(
-                                    (hoveredRow as MRT_Row<PlayerJson>).index,
-                                    0,
-                                    rankings.splice(draggingRow.index, 1)[0]
-                                );
-                                setRankings([...rankings]);
+                            // if dragging FROM rankings TO remaining players
+                            if (hoveredTable === Table.RemainingPlayers) {
+                                if (draggingRow) {
+                                    rankings.splice(draggingRow.index, 1);
+                                    setRankings([...rankings]);
+                                    const rowToBeInserted =
+                                        draggingRow.original;
+
+                                    if (!hoveredRow) {
+                                        setRemainingPlayers(remainingPlayers =>
+                                            remainingPlayers.length === 0
+                                                ? [rowToBeInserted]
+                                                : [
+                                                      ...remainingPlayers,
+                                                      rowToBeInserted,
+                                                  ]
+                                        );
+                                    } else {
+                                        remainingPlayers.splice(
+                                            hoveredRow.index,
+                                            0,
+                                            rowToBeInserted
+                                        );
+                                        setRemainingPlayers([
+                                            ...remainingPlayers,
+                                        ]);
+                                    }
+                                }
+                            } else if (hoveredTable === Table.Rankings) {
+                                // dragging within rankings table
+                                const {draggingRow, hoveredRow} =
+                                    table.getState();
+                                if (hoveredRow && draggingRow) {
+                                    rankings.splice(
+                                        (hoveredRow as MRT_Row<PlayerJson>)
+                                            .index,
+                                        0,
+                                        rankings.splice(draggingRow.index, 1)[0]
+                                    );
+                                    setRankings([...rankings]);
+                                }
                             }
+
+                            setHoveredTable(Table.None);
+                        },
+                    }),
+                    muiTablePaperProps: ({table}) => ({
+                        onDragEnter: () => {
+                            setHoveredTable(Table.Rankings);
+                            const {hoveredRow} = table.getState();
+                            setHoveredRow(hoveredRow as MRT_Row<PlayerJson>);
+                        },
+                        sx: {
+                            outline:
+                                hoveredTable === Table.Rankings
+                                    ? '2px dashed pink'
+                                    : undefined,
+                        },
+                    }),
+                })}
+            />
+        );
+    }
+
+    function remainingPlayersComponent() {
+        return (
+            <MaterialReactTable
+                table={useMaterialReactTable({
+                    autoResetPageIndex: false,
+                    columns,
+                    data: remainingPlayers,
+                    enableRowOrdering: true,
+                    enableSorting: true,
+                    renderTopToolbarCustomActions: () => (
+                        <Typography variant="h5">Remaining Players</Typography>
+                    ),
+                    onDraggingRowChange: setDraggingRow,
+                    state: {draggingRow},
+                    muiRowDragHandleProps: ({table}) => ({
+                        onDragStart: () => {
+                            setHoveredTable(Table.RemainingPlayers);
+                        },
+                        onDragEnd: () => {
+                            // if dragging FROM remaining players TO rankings
+                            if (hoveredTable === Table.Rankings) {
+                                if (draggingRow) {
+                                    remainingPlayers.splice(
+                                        draggingRow.index,
+                                        1
+                                    );
+                                    setRemainingPlayers([...remainingPlayers]);
+                                    const rowToBeInserted =
+                                        draggingRow.original;
+
+                                    if (!hoveredRow) {
+                                        setRankings(rankings =>
+                                            rankings.length === 0
+                                                ? [rowToBeInserted]
+                                                : [...rankings, rowToBeInserted]
+                                        );
+                                    } else {
+                                        rankings.splice(
+                                            hoveredRow.index,
+                                            0,
+                                            rowToBeInserted
+                                        );
+                                        setRankings([...rankings]);
+                                    }
+                                }
+                            } else if (
+                                hoveredTable === Table.RemainingPlayers
+                            ) {
+                                const {draggingRow, hoveredRow} =
+                                    table.getState();
+                                if (hoveredRow && draggingRow) {
+                                    remainingPlayers.splice(
+                                        (hoveredRow as MRT_Row<PlayerJson>)
+                                            .index,
+                                        0,
+                                        remainingPlayers.splice(
+                                            draggingRow.index,
+                                            1
+                                        )[0]
+                                    );
+                                    setRemainingPlayers([...remainingPlayers]);
+                                }
+                            }
+
+                            setHoveredTable(Table.None);
+                        },
+                    }),
+                    muiTablePaperProps: ({table}) => ({
+                        onDragEnter: () => {
+                            setHoveredTable(Table.RemainingPlayers);
+                            const {hoveredRow} = table.getState();
+                            setHoveredRow(hoveredRow as MRT_Row<PlayerJson>);
+                        },
+                        sx: {
+                            outline:
+                                hoveredTable === Table.RemainingPlayers
+                                    ? '2px dashed pink'
+                                    : undefined,
                         },
                     }),
                 })}
@@ -145,8 +343,13 @@ export default function FootballRanker() {
             <div className="toolbar">
                 {downloadButton()}
                 {uploadButton()}
+                {startNewButton()}
+                {remainingPlayersToggle()}
             </div>
-            <div className="ranker">{rankingComponent()}</div>
+            <div className="ranker">
+                {rankingComponent()}
+                {remainingPlayersComponent()}
+            </div>
         </div>
     );
 }
